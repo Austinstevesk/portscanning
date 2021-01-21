cd890319cb6ce8fda8d00523dd0710fda325625b
Austinstevesk <austinstevesk@gmail.com>
2021-01-21 06:28:04 +0300

    Prompting user input

--- portscanner.py
+++ portscanner.py
@@ -3,9 +3,12 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ports = []
 
-host = "192.168.100.72"
-ports = 445,902,3306
+host = input("[*]Enter the host to scan: ")
+number_of_ports = int(input("Enter the number of ports to scan: "))
+for i in range(0,number_of_ports):
+	ports.append(int(input("Enter port: ")))
 
 def portScanner(args):
 	for port in ports:
