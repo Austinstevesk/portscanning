045e37157019849bce230f99be2170468c936177
Austinstevesk <austinstevesk@gmail.com>
2021-01-21 05:45:13 +0300

    Multiple ports

--- portscanner.py
+++ portscanner.py
@@ -5,13 +5,14 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "192.168.100.72"
-port = 445
+ports = 445,902,3306
 
-def portScanner(port):
-	if sock.connect_ex((host, port)):
-		print("Port ",port, " is closed")
-	else:
-		print("Port ",port, " is opened")
+def portScanner(args):
+	for port in ports:
+		if sock.connect_ex((host, port)):
+			print("Port ",port, " is closed")
+		else:
+			print("Port ",port, " is opened")
 
 
-portScanner(port)
+portScanner(ports)
