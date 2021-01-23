dfaada5a8a275665356dde6abc67e7a92e8dce2a
Austinstevesk <austinstevesk@gmail.com>
2021-01-23 20:27:54 +0300

    Resolved names and could scan multiple ports

--- advanced_scanner.py
+++ advanced_scanner.py
@@ -1,13 +1,47 @@
 #!/usr/bin/python
 
+
+#Running the program
+#1. python advanced_scanner.py -H AS -p 135,110,80,50,3306 -> Name is resolved
+#2. python advanced_scanner.py -H 192.168.10.10 -p 135,110,80,50,3306
+#3. python advanced_scanner.py -H google.com -p 135,110,80,50,3306
+
+
 from socket import *
 import optparse
 from threading import *
 
-def portScan 
+
+def connScan(tgtHost, tgtPort): #Returns whether the port is closed or open
+	try:
+		sock = socket(AF_INET, SOCK_STREAM)
+		sock.connect((tgtHost, tgtPort))
+		print("[+] tcp Open ", tgtPort)
+	except:
+		print("[-] tcp Closed ", tgtPort)
+
+	finally:
+		sock.close()
+
+
+def portScan(tgtHost, tgtPorts): #Resolves names and addresses
+	try:
+		tgtIP = gethostbyname(tgtHost) #Resolves name
+	except:
+		print("Unknown target host", tgtHost)
+	try:
+		tgtName = gethostbyaddr(tgtIP) 
+		print("[+] Scan results for: ", tgtName[0])
+	except:
+		print("[+] Scan results for: ", tgtIP)
+
+		setdefaulttimeout(1)
+	for tgtPort in tgtPorts:
+		t = Thread(target=connScan, args=[tgtHost, int(tgtPort)])
+		t.start()
 
 def main():
-	parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports>')
+	parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports>') #Prints program usage
 	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
 	parser.add_option('-p', dest='tgtPort', type='string', help='specify target ports separated by comma')
 	(options, args) = parser.parse_args()
@@ -17,6 +51,6 @@ def main():
 	if (tgtHost == None or (tgtPorts[0]) == None):
 		print(parser.usage)
 		exit(0)
-		#portScan(tgtHost, tgtPorts)
+	portScan(tgtHost, tgtPorts)
 if __name__ == '__main__':
 	main()
